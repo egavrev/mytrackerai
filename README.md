1c89a3bd371b38baca225975144b474a71961096
Eugeniu Gavrev <gavreveugen@gmail.com>
2024-02-10 18:43:10 +0200

    fix smile short codes

--- README.md
+++ README.md
@@ -34,7 +34,7 @@ Try to rebuild it using Code Interpretor and assistants.
 - [X] Clean all topics/domains/sensations. 
 - [X] Configuration add CRUD
 - [X] show tables entries with icons and in sentiment colors
-- [ ] emoji codes not displayed after load from DB https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/
+- [X] emoji codes not displayed after load from DB https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/
 - [ ] On changing date refresh all for week in which this date is located. 
 - [ ] Add planning for the self development activity.
 - [ ] print your status for weekly activity in case you are reaching targets. 
--- journal.py
+++ journal.py
@@ -88,11 +88,12 @@ def app():
 
     if entries:
         for entry in entries:
-            cols = st.columns([0.5, 0.5, 2.5, 0.5],gap="small")
+            cols = st.columns([0.5, 0.2, 2.5, 0.5],gap="small")
             color = sentiments_list[entry.sentiment_id-1][3]
             #cols[0].markdown(f"<div style='background-color:{color}; padding:10px;'>{entry.date}</div>", unsafe_allow_html=True)
-            cols[0].markdown(f"<div style='background-color:{color}; padding:10px;'>{domains_list[entry.domain_id-1][1]}</div>", unsafe_allow_html=True)
-            cols[1].markdown(f"<div style='background-color:{color}; padding:10px;'>{sentiments_list[entry.sentiment_id-1][2]}</div>", unsafe_allow_html=True)
-            cols[2].markdown(f"<div style='background-color:{color}; padding:10px;'>{entry.description}</div>", unsafe_allow_html=True)
+            cols[0].markdown(f"<div style='background-color:{color}; padding:10px;'><strong style='color:black;'>{domains_list[entry.domain_id-1][1]}</strong></div>", unsafe_allow_html=True)
+            #cols[1].markdown(f"<div style='background-color:{color}; padding:10px;'>{sentiments_list[entry.sentiment_id-1][2]}</div>", unsafe_allow_html=True)
+            cols[1].markdown(sentiments_list[entry.sentiment_id-1][2])
+            cols[2].markdown(f"<div style='background-color:{color}; padding:10px;'><strong style='color:black;'>{entry.description}</strong></div>", unsafe_allow_html=True)
             if cols[3].button("Delete", key=entry.entry_id):
                 delete_journal_entry(entry.entry_id)
